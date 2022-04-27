4cc5897a8741b2b4c7517773b775c414b095ad03
Ryan Benson <benson.ryan@gmail.com>
2022-04-27 16:44:53 -0500

    Got react working inside of express and build process

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
