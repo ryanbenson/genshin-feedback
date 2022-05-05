fd3e4adf8d9fce55a210709d9d239fffdc4f6e7d
Ryan Benson <benson.ryan@gmail.com>
2022-05-05 16:23:19 -0500

    Testing model usage in a route

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const passport = require("passport");
 const path = require("path");
 const router = express.Router();
-// s * min * h * days
-const cookieTime = 60000 * 60 * 24 * 30;
+const { User } = require("../utils/database");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   console.log(req.cookies);
   const cookieVal = req.cookies?.name ?? null;
   if (!cookieVal || cookieVal !== "express") {
-    res.render("index", { title: "Express" });
+    User.findAll().then((users) => res.json(users));
+    //res.render("index", { title: "Express" });
   } else {
     res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
   }
