e090fd9a3ccc7bcd9efae0cc592a52cb5062a6c7
Ryan Benson <benson.ryan@gmail.com>
2022-05-06 14:30:22 -0500

    Implements successful login in app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser");
 var session = require("express-session");
 const logger = require("morgan");
 const { Sequelize } = require("sequelize");
-const { db } = require("./utils/database");
+const { db, User } = require("./utils/database");
 
 (async () => {
   const initDatabase = async () => {
@@ -66,46 +66,17 @@ passport.use(
       clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
       callbackURL: "http://localhost:3001/oauth2/redirect/google",
     },
-    function (issuer, profile, cb) {
+    async function (issuer, profile, cb) {
       console.log(issuer, profile);
-      return cb(null, { issuer, profile });
-      // db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-      //   issuer,
-      //   profile.id
-      // ], function(err, cred) {
-      //   if (err) { return cb(err); }
-      //   if (!cred) {
-      //     // The Google account has not logged in to this app before.  Create a
-      //     // new user record and link it to the Google account.
-      //     db.run('INSERT INTO users (name) VALUES (?)', [
-      //       profile.displayName
-      //     ], function(err) {
-      //       if (err) { return cb(err); }
-
-      //       var id = this.lastID;
-      //       db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-      //         id,
-      //         issuer,
-      //         profile.id
-      //       ], function(err) {
-      //         if (err) { return cb(err); }
-      //         var user = {
-      //           id: id.toString(),
-      //           name: profile.displayName
-      //         };
-      //         return cb(null, user);
-      //       });
-      //     });
-      //   } else {
-      //     // The Google account has previously logged in to the app.  Get the
-      //     // user record linked to the Google account and log the user in.
-      //     db.get('SELECT * FROM users WHERE id = ?', [ cred.user_id ], function(err, user) {
-      //       if (err) { return cb(err); }
-      //       if (!user) { return cb(null, false); }
-      //       return cb(null, user);
-      //     });
-      //   }
-      // };
+      const [user, created] = await User.findOrCreate({
+        where: {
+          profileId: profile?.id,
+          // issuer will be https://accounts.google.com
+          profilePlatform: issuer,
+        },
+      });
+      console.log(created ? "## User created" : "## User found");
+      return cb(null, user);
     }
   )
 );
