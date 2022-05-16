72bb2f11dbf0acfc51d05a41cc78feba948d7682
Ryan Benson <benson.ryan@gmail.com>
2022-05-16 15:08:30 -0500

    Adds sample API call

--- frontend/src/features/counter/counterAPI.js
+++ frontend/src/features/counter/counterAPI.js
@@ -1,6 +1,8 @@
 // A mock function to mimic making an async request for data
-export function fetchCount(amount = 1) {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve({ data: amount }), 500)
-  );
+export function fetchCount() {
+  return new Promise((resolve) => {
+    return fetch("http://localhost:3001/api/feedback")
+      .then((response) => response.json())
+      .then((data) => resolve(data));
+  });
 }
