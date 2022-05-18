683bfc8caad9adef23a2752c01a0ac79cc9543f9
Ryan Benson <benson.ryan@gmail.com>
2022-05-18 16:40:00 -0500

    Adds axios, updates API call to get feedback

--- frontend/src/features/counter/counterAPI.js
+++ frontend/src/features/counter/counterAPI.js
@@ -1,8 +1,14 @@
+import axios from "axios";
 // A mock function to mimic making an async request for data
 export function fetchCount() {
-  return new Promise((resolve) => {
-    return fetch("http://localhost:3001/api/feedback")
-      .then((response) => response.json())
-      .then((data) => resolve(data));
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:3001/api/feedback", { withCredentials: true })
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
   });
 }
