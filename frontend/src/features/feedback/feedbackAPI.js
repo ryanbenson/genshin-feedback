bcbef7068582416d6ffaf0021c8cd613f1186ee1
Ryan Benson <benson.ryan@gmail.com>
2022-05-23 16:13:51 -0500

    Adds new feedback store

---
+++ frontend/src/features/feedback/feedbackAPI.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export function getFeedback() {
+  return new Promise((resolve, reject) => {
+    axios
+      .get("http://localhost:3001/api/feedback", { withCredentials: true })
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
+
+export function createFeedback(content) {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(
+        "http://localhost:3001/api/feedback",
+        { content: "hello" },
+        { withCredentials: true }
+      )
+      .then(function (response) {
+        resolve(response);
+      })
+      .catch(function (error) {
+        reject(error);
+      });
+  });
+}
