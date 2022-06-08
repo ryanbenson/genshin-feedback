965e31c9d38990c18ce3f852d7865457b772a6ab
Ryan Benson <benson.ryan@gmail.com>
2022-06-08 17:32:27 -0500

    Adds feedback save store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import feedbackReducer from "../features/feedback/feedbackSlice";
 import feedbackLikeReducer from "../features/feedback/feedbackLikeSlice";
+import feedbackSaveReducer from "../features/feedback/feedbackSaveSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     feedback: feedbackReducer,
     feedbackLike: feedbackLikeReducer,
+    feedbackSave: feedbackSaveReducer,
   },
 });
